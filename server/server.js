dd5bd75d0e694681b2c8afd7a99855f74fd8653a
ibrahim735 <adeniranadekunle152@gmail.com>
2024-11-21 15:47:11 +0100

    little backend code for the server and authentication added

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const spotifyWebApi = require("spotify-web-api-node")
+
+const app = express();
+
+app.post('./login', (req, res) => {
+    const code = req.body.code;
+    const spotifyApi = new spotifyWebApi({
+        clientId: '124ef34fb5224fdf868a397490a31774',
+        clientSecret: 'a442ce8e7ca4402a82c8972f6c91b3ac',
+        redirectUri: 'http://localhost:5175/'
+    });
+
+    spotifyApi.authorizationCodeGrant(code)
+       .then(data => {
+            res.json({
+                accessToken: data.body.access_token,
+                refreshToken: data.body.refresh_token,
+                expiresIn: data.body.expires_in
+            });
+        })
+       .catch(() => {
+            res.sendStatus(400)
+        });
+})
